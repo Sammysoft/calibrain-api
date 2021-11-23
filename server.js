411f44bf2fa2bddaaeb561ac2f0a996f6f2a08ad
Samuel Dare-Owonibi <dareowonibisamuel@gmail.com>
2021-11-23 11:41:24 +0000

    oga oooo

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 // const cors = require('cors');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const server = express();
 // require('dotenv').config();
-const userAPI = require('./controller/user.controller')
-const staffAPI = require('./controller/staff-controller')
-const subjectAPI = require('./controller/subjects-controller')
-const authAPI = require('./controller/auth-controller');
+// const userAPI = require('./controller/user.controller')
+// const staffAPI = require('./controller/staff-controller')
+// const subjectAPI = require('./controller/subjects-controller')
+// const authAPI = require('./controller/auth-controller');
 // require('./api/db')
 // let Mongo = process.env.MongoURI
 // server.use(express.json());
@@ -18,9 +18,9 @@ server.listen(port, ()=>{
 // server.use(bodyParser.json())
 // server.use(bodyParser.urlencoded({extended: true}))
 // server.use(cors());
-// server.get('/', (req,res,next)=>{
-//     res.status(200).json('Welcome To Calibrain RESTful API service...')
-// });
+server.get('/', (req,res,next)=>{
+    res.status(200).json('Welcome To Calibrain RESTful API service...')
+});
 // server.use('/api', userAPI)
 // server.use('/api', staffAPI)
 // server.use('/api', subjectAPI)
