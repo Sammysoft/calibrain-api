6c8be64133036d25352f6ddc548ea40086ad3f51
Samuel Dare-Owonibi <dareowonibisamuel@gmail.com>
2021-11-23 16:53:09 +0000

    deploying some changes

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const server = express();
+const app = express();
 require('dotenv').config();
 const userAPI = require('./controller/user.controller')
 const staffAPI = require('./controller/staff-controller')
 const subjectAPI = require('./controller/subjects-controller')
 const authAPI = require('./controller/auth-controller');
 require('./api/db');
-server.use(express.json());
+app.use(express.json());
+const port = process.env.PORT || 5056;
 
-
-server.use(bodyParser.json())
-server.use(bodyParser.urlencoded({extended: true}))
-server.use(cors());
-server.get('/', (req,res,next)=>{
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(cors());
+app.get('/', (req,res,next)=>{
     res.status(200).json('Welcome To Calibrain RESTful API service...')
 });
-server.use('/api', userAPI)
-server.use('/api', staffAPI)
-server.use('/api', subjectAPI)
-server.use('/api', authAPI)
+app.use('/api', userAPI)
+app.use('/api', staffAPI)
+app.use('/api', subjectAPI)
+app.use('/api', authAPI)
 
-const port = process.env.PORT || 5056;
-server.listen(port, ()=>{
-    console.log(`Server connected on port ${port}` );
+
+app.listen(port, ()=>{
+    console.log(`app connected on port ${port}` );
 })
