733b4b6e60b7befe14560a6daa659b85604ae0a7
Samuel Dare-Owonibi <dareowonibisamuel@gmail.com>
2021-11-24 13:34:02 +0000

    proxy

--- api/user.js
+++ api/user.js
@@ -20,7 +20,7 @@ module.exports = {
             }else{
                 const user = await new User( req.body.user )
                 user.save()
-                res.status(200).json( user )
+                res.status(200)
             }
         }catch(error){
             res.status(400).json({errors: {global: 'Could not register'}})
