155f3ed6b41709a0a1289333d4bb9efae7cd1b0a
Samuel Dare-Owonibi <dareowonibisamuel@gmail.com>
2022-02-18 16:32:25 +0000

    more commits

--- controller/staff-controller.js
+++ controller/staff-controller.js
@@ -6,7 +6,7 @@ const getAllStaffs = require('../api/staff');
 const getAStaff = require('../api/staff');
 const { verifyTokenAndAuth, verifyTokenAndAdmin } = require('../middleware/verifyToken')
 
-router.post('/addStaff',  addStaff._addStaff);
+router.post('/addStaff', verifyTokenAndAuth, verifyTokenAndAdmin,  addStaff._addStaff);
 router.post('/removeStaff/:id', verifyTokenAndAuth, verifyTokenAndAdmin, removeStaff._removeStaff);
 router.get('/getAllStaffs',  getAllStaffs._getAllStaffs);
 router.post('/getAStaff/:id', verifyTokenAndAuth, getAStaff._getAStaff)
